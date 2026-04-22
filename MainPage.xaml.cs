ddd2e496aa4556220f3a5175c64f0b4bea07b73b
José Yañez <57007976+MasterJayanX@users.noreply.github.com>
2026-04-22 17:11:56 -0400

    Reposicionamiento del título + cambios en el manifest

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,17 +6,23 @@ public partial class MainPage : ContentPage
 
 	public MainPage()
 	{
+		// Función de inicialización de la aplicación. El color inicial es negro.
 		InitializeComponent();
+		RedSlider.Value = 0;
+		GreenSlider.Value = 0;
+		BlueSlider.Value = 0;
 		EstablecerColor();
 	}
 
 	private void Slider_ValueChanged(object? sender, ValueChangedEventArgs e)
 	{
+		// Actualizar el color cuando cambia el valor de alguno de los sliders
 		EstablecerColor();
 	}
 
 	private void RandomButton_Clicked(object? sender, EventArgs e)
 	{
+		// Generar valores aleatorios para los sliders
 		RedSlider.Value = _random.Next(0, 256);
 		GreenSlider.Value = _random.Next(0, 256);
 		BlueSlider.Value = _random.Next(0, 256);
@@ -26,11 +32,13 @@ public partial class MainPage : ContentPage
 
 	private async void Clipboard_Clicked(object? sender, EventArgs e)
 	{
+		// Copiar el valor hexadecimal al portapapeles
 		await Clipboard.Default.SetTextAsync(HexValueLabel.Text);
 	}
 
 	private void EstablecerColor()
 	{
+		// Obtener los valores de los sliders y crear un color a partir de ellos
 		int red = (int)RedSlider.Value;
 		int green = (int)GreenSlider.Value;
 		int blue = (int)BlueSlider.Value;
@@ -40,7 +48,6 @@ public partial class MainPage : ContentPage
 
 		ColorContainer.BackgroundColor = color;
 		HexValueLabel.Text = $"#{red:X2}{green:X2}{blue:X2}";
-		TitleLabel.TextColor = textColor;
 		RedValueLabel.TextColor = textColor;
 		GreenValueLabel.TextColor = textColor;
 		BlueValueLabel.TextColor = textColor;
@@ -49,7 +56,8 @@ public partial class MainPage : ContentPage
 
 	private static Color ObtenerColorTextoPorContraste(int red, int green, int blue)
 	{
-		// Formula de luminancia perceptual para decidir negro/blanco.
+		// Esta función calcula la luminancia del color y devuelve negro o blanco 
+		// dependiendo de cuál tenga mejor contraste con el fondo.
 		double luminancia = (0.299 * red) + (0.587 * green) + (0.114 * blue);
 		return luminancia > 186 ? Colors.Black : Colors.White;
 	}
--- obj/Debug/net10.0-android/Proyecto1Colores.AssemblyInfo.cs
+++ obj/Debug/net10.0-android/Proyecto1Colores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1Colores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3aa582f69f07552cb84868e825975dc6228f3b0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7858a9245961f168702b7899fe6cbd98f43a5861")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1Colores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1Colores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
